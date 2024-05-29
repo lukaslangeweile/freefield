2ab1abb16adaf943c996bddb34fc721802b89b16
Lukas Lange <150951654+lukaslangeweile@users.noreply.github.com>
2024-05-29 12:38:25 +0200

    new flush buffers iteration

--- freefield/freefield.py
+++ freefield/freefield.py
@@ -378,12 +378,11 @@ def flush_buffers(processor, maximum_n_samples=80000):
                         "cathedral_play_buf.rcx": 8}
     circuit = os.path.basename(PROCESSORS.rcx_dict.get(processor))
 
-    PROCESSORS.write(tag="playbuflen", value=1, processors=processor)
     if n_buffer_dict.get(circuit) == 1:
         PROCESSORS.write(tag="data", value=np.zeros(maximum_n_samples), processors=processor)
     else:
         for i in range(n_buffer_dict.get(circuit)):
-            if PROCESSORS.read(tag=f"data{i}", proc=processor) != 0:
+            if not np.all(PROCESSORS.read(tag=f"data{i}", proc=processor, n_samples=1000) == 0):
                 PROCESSORS.write(tag=f"data{i}", value=np.zeros(maximum_n_samples), processors=processor)
 
 def play_and_record(speaker, sound, compensate_delay=True, compensate_attenuation=False, equalize=False,
